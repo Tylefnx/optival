7be8648be78fdff43e3411687c5d981082a5168d
tylefnx@gen2 <tayfunucuncu@gmail.com>
2024-07-08 10:23:55 +0300

    required administration in windows

--- lib/main_screen/core/srm_3006_connection_service.dart
+++ lib/main_screen/core/srm_3006_connection_service.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:flutter_libserialport/flutter_libserialport.dart';
 import 'package:optival/main_screen/core/shared/int2str_converter.dart';
 
